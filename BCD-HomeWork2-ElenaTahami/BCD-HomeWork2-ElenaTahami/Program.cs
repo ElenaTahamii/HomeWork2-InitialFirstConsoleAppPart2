3525dbd5968d12901383d29994230e9b5a9b1706
Elena Tahami <OPERA@DESKTOP-9GFD52L>
2026-06-12 18:15:59 +0330

    daneshkar-ElenaTahami-home work2

---
+++ BCD-HomeWork2-ElenaTahami/BCD-HomeWork2-ElenaTahami/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+class Student
+{
+    private string name = "";
+    private int age;
+    public string Name
+    {
+        get { return name; }
+        set { name = value; }
+    }
+
+    public int Age
+    {
+        get { return age; }
+        set { age = value; }
+    }
+
+    public void Introduce()
+
+    {
+        Console.WriteLine($" hello My Name is  {Name} and I am  {Age}  years old");
+    }
+}
+
+class Program
+{
+    static void Main()
+
+    {
+
+        Student p = new Student();
+        p.Name = "Elena";
+        p.Age = 33;
+        p.Introduce();
+    }
+
+}
